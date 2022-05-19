2d34bf65b904f98515734e4cfff8895c387f3387
Murad <muradteymurov2000@gmail.com>
2022-05-19 09:58:30 +0400

    loadedRouter

--- src/Page/Notfound.js
+++ src/Page/Notfound.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function NotFound(){
     return(
-        <h1>nothing found <span>404 error</span></h1>
+        <h1>Anything NotFound <span>404 error</span></h1>
     )
 } 
